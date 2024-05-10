b6530d81c9fbddf60c56bd5be68a630efbf97bd7
MostakemHossain <mostakememon123@gmail.com>
2024-05-10 11:09:53 +0600

    filter in Admin added

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -1,14 +1,25 @@
 import { Prisma, PrismaClient } from "@prisma/client";
+import { adminSearchAbleFields } from "./admin.constant";
 
 const prisma = new PrismaClient();
 const getAllAdmin = async (params: any) => {
+  const { searchTerm, ...filterData } = params;
   const andConditions: Prisma.AdminWhereInput[] = [];
   if (params.searchTerm) {
     andConditions.push({
-      OR: ["name", "email"].map((field) => ({
+      OR: adminSearchAbleFields.map((field) => ({
         [field]: {
           contains: params.searchTerm,
           mode: "insensitive",
+        },
+      })),
+    });
+  }
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: filterData[key],
         },
       })),
     });
