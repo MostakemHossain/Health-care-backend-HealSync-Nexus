2d2c9eff38639dfe290671b012f944133c64cd10
MostakemHossain <mostakememon123@gmail.com>
2024-05-10 20:01:39 +0600

    router file created in a single file

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
-import { adminRoutes } from "./app/modules/admin/admin.routes";
-import { userRoutes } from "./app/modules/user/user.routes";
+import router from "./app/routes";
 const app: Application = express();
 
 //parser
@@ -13,12 +12,11 @@ app.use(
   })
 );
 
-app.use("/api/v1/user", userRoutes);
-app.use("/api/v1/admin", adminRoutes);
 app.get("/", (req: Request, res: Response) => {
   res.send({
     message: "HealSync Nexus server.......",
   });
 });
+app.use("/api/v1", router);
 
 export default app;
---
+++ src/app/routes/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { adminRoutes } from "../modules/admin/admin.routes";
+import { userRoutes } from "../modules/user/user.routes";
+const router = express.Router();
+
+const moduleRoutes = [
+  {
+    path: "/user",
+    route: userRoutes,
+  },
+  {
+    path: "/admin",
+    route: adminRoutes,
+  },
+];
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
